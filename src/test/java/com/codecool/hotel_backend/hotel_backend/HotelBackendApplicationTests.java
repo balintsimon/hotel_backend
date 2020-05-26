9a9ad4a982adb54d5c6c10e3c9ada22028a52217
kalnaipeter <kalnaipeter@gmail.com>
2020-05-26 10:37:49 +0200

    start

--- src/test/java/com/codecool/hotel_backend/hotel_backend/HotelBackendApplicationTests.java
+++ src/test/java/com/codecool/hotel_backend/hotel_backend/HotelBackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.hotel_backend.demo;
+package com.codecool.hotel_backend.hotel_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class HotelBackendApplicationTests {
 
     @Test
     void contextLoads() {
