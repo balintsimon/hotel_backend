1e693738c267a990775938c07844b79eb9a9a15e
Gnorbi951 <gnorbi951@gmail.com>
2020-05-27 11:21:58 +0200

    add new field, and start counters from 1

--- src/main/java/com/codecool/hotel_backend/model/SuperiorStreetView.java
+++ src/main/java/com/codecool/hotel_backend/model/SuperiorStreetView.java
@@ -3,7 +3,8 @@ package com.codecool.hotel_backend.model;
 public class SuperiorStreetView extends Category{
 
     public SuperiorStreetView() {
-        description = "This is a SuperiorStreetView room";
+        name = "Superior Street View Room";
+        description = "Description for superior room";
         capacity = 2;
         size = 15;
     }
