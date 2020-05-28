8f626977835e59016d66ef96a6cb1c3290a44fdb
Gnorbi951 <gnorbi951@gmail.com>
2020-05-28 11:28:11 +0200

    Add descriptions to categories

--- src/main/java/com/codecool/hotel_backend/model/Luxury.java
+++ src/main/java/com/codecool/hotel_backend/model/Luxury.java
@@ -5,7 +5,7 @@ public class Luxury extends Category{
     public Luxury(){
         id=1;  
         name = "Luxury Room";
-        description = "Description for luxury room";
+        description = "These rooms provide you not only great views of the city but space to relax with your friends and family, while enjoying a special welcome amenity.";
         imgUrl = "https://pix10.agoda.net/hotelImages/160/1601291/1601291_17062916290054159438.jpg?s=1024x768";
         capacity = 6;
         size = 25;
--- src/main/java/com/codecool/hotel_backend/model/RockstarSuite.java
+++ src/main/java/com/codecool/hotel_backend/model/RockstarSuite.java
@@ -6,7 +6,7 @@ public class RockstarSuite extends Category{
         
         id=3;
         name = "Rockstar Suite Room";
-        description = "Description for rockstar suite";
+        description = "Our trademark Rock Star Suite is the perfect choice for the after party - minibar included. Have your crew over to chill or enjoy some great bites on your own terrace, then let good times flow in your own jacuzzi.";
         imgUrl = "https://www.dertourluxury.hu/upload/pics/hotel/gallery/C__WebServ_Webs_Fundacion_folletos_download_742414550_HardRock%20HardRock%20RockstarSuite_Foyerk.jpg";
         capacity = 4;
         size = 20;
