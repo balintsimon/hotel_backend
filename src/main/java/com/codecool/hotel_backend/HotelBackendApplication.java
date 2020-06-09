b72b350ee4100438a53e8c90b5b5aab4806d6de4
dr. Simon Bálint <balladin0@gmail.com>
2020-06-09 08:03:22 +0200

    add all room categories with names

--- src/main/java/com/codecool/hotel_backend/HotelBackendApplication.java
+++ src/main/java/com/codecool/hotel_backend/HotelBackendApplication.java
@@ -25,13 +25,34 @@ public class HotelBackendApplication {
         return args -> {
             Category luxury = Category.builder()
                     .Id(1L)
+                    .name("Luxury Room")
                     .capacity(6L)
                     .description("These rooms provide you not only great views of the city but space to relax with your friends and family, while enjoying a special welcome amenity.")
                     .imgUrl("https://pix10.agoda.net/hotelImages/160/1601291/1601291_17062916290054159438.jpg?s=1024x768")
                     .size(25L)
                     .build();
 
+            Category SuperiorStreetView = Category.builder()
+                    .Id(2L)
+                    .name("Superior Street View Room")
+                    .capacity(2L)
+                    .description("Take in all what Budapest has to offer while staying close to all the key acts in the city.")
+                    .imgUrl("https://q-xx.bstatic.com/xdata/images/hotel/840x460/183479087.jpg?k=cf9f2554a81a9bc9e5e661af0fe034e09ec867dfa90112a6668afe590eddec31&o=")
+                    .size(15L)
+                    .build();
+
+            Category RockstarSuite = Category.builder()
+                    .Id(3L)
+                    .name("Rockstar Suite Room")
+                    .capacity(4L)
+                    .description("Our trademark Rock Star Suite is the perfect choice for the after party - minibar included. Have your crew over to chill or enjoy some great bites on your own terrace, then let good times flow in your own jacuzzi.")
+                    .imgUrl("https://www.dertourluxury.hu/upload/pics/hotel/gallery/C__WebServ_Webs_Fundacion_folletos_download_742414550_HardRock%20HardRock%20RockstarSuite_Foyerk.jpg")
+                    .size(20L)
+                    .build();
+
             categoryRepository.save(luxury);
+            categoryRepository.save(SuperiorStreetView);
+            categoryRepository.save(RockstarSuite);
         };
 
     }
