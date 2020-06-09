b80b4aa7af20686fab30101e5c6ebf1899989bac
dr. Simon Bálint <balladin0@gmail.com>
2020-06-09 11:37:10 +0200

    add reservation-related initial classes

---
+++ src/main/java/com/codecool/hotel_backend/controller/ReservationController.java
@@ -0,0 +1,42 @@
+package com.codecool.hotel_backend.controller;
+
+import com.codecool.hotel_backend.model.Room;
+import com.codecool.hotel_backend.repository.CategoryRepository;
+import com.codecool.hotel_backend.repository.ReservationRepository;
+import com.codecool.hotel_backend.service.RoomStorage;
+import jdk.vm.ci.meta.Local;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.method.P;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+public class ReservationController {
+
+    ReservationRepository reservationRepository;
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    public ReservationController(CategoryRepository categoryRepository, ReservationRepository reservationRepository) {
+        this.categoryRepository = categoryRepository;
+        this.reservationRepository = reservationRepository;
+    }
+
+//    // TODO: switch to H2
+//    @RequestMapping(value = "/category/reserve/{id}", method = RequestMethod.POST)
+//    public Room reserveARoomByCategoryId(@PathVariable("id") int id) {
+//        return roomStorage.reserveARoom(id);
+//    }
+
+    @RequestMapping(value = "/category/reserve/{id}/{start}/{end}", method = RequestMethod.POST)
+    public boolean reserveACategoryById(@PathVariable("id") Long id,
+                                        @PathVariable("start") String start,
+                                        @PathVariable("end") String end) {
+
+        LocalDate startTime = LocalDate.parse(start);
+        LocalDate endTime = LocalDate.parse(end);
+
+        return true;
+    }
+}
--- src/main/java/com/codecool/hotel_backend/repository/ReservationRepository.java
+++ src/main/java/com/codecool/hotel_backend/repository/ReservationRepository.java
@@ -1,7 +1,32 @@
 package com.codecool.hotel_backend.repository;
 
+import com.codecool.hotel_backend.entity.Category;
 import com.codecool.hotel_backend.entity.Reservation;
+import com.codecool.hotel_backend.entity.ReservedRoom;
+import lombok.EqualsAndHashCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.CascadeType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import java.time.LocalDate;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+//    private Long id;
+//    private Category category;
+//    private LocalDate startDate;
+//    private LocalDate endDate;
+//    private ReservedRoom reservedRoom;
+
+//    @Query(value = "update Address a set a.country = 'USA' where a.id in " +
+//            "(select s.address.id from Student s where s.name LIKE :name)")
+//    @Modifying(clearAutomatically = true)
+
+
+    @Query(value = "UPDATE Reservation r SET ")
+    boolean enterNewReservation(Long id, LocalDate startTime, LocalDate endTime);
+
 }
