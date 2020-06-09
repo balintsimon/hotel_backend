ebf9352950c505adace8fd7d980c8cc152233021
dr. Simon Bálint <balladin0@gmail.com>
2020-06-09 08:49:25 +0200

    add basic queries for testing

--- src/main/java/com/codecool/hotel_backend/repository/CategoryRepository.java
+++ src/main/java/com/codecool/hotel_backend/repository/CategoryRepository.java
@@ -2,6 +2,15 @@ package com.codecool.hotel_backend.repository;
 
 import com.codecool.hotel_backend.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Category findCategoryById(int id);
+
+    @Query(value = "SELECT c FROM Category c")
+    List<Category> getCategoryStorage();
+    
 }
