9edb7ecdedfb9ecea4f8c7dbfa4e102622498075
dr. Simon Bálint <balladin0@gmail.com>
2020-06-09 10:42:01 +0200

    format code

--- src/main/java/com/codecool/hotel_backend/controller/CategoryController.java
+++ src/main/java/com/codecool/hotel_backend/controller/CategoryController.java
@@ -12,27 +12,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
-    RoomStorage roomStorage;
+    RoomStorage roomStorage; // TODO: switch to H2
     CategoryRepository categoryRepository;
 
     @Autowired
-    public CategoryController(CategoryRepository categoryRepository,RoomStorage roomStorage){
+    public CategoryController(CategoryRepository categoryRepository, RoomStorage roomStorage) {
         this.categoryRepository = categoryRepository;
         this.roomStorage = roomStorage;
     }
 
     @GetMapping("/{id}")
-    public Category getAllByCategory(@PathVariable("id") Long id){
+    public Category getAllByCategory(@PathVariable("id") Long id) {
         return categoryRepository.findCategoryByCategoryId(id);
     }
 
     @GetMapping("/all")
-    public List<Category> getAllCategory(){
+    public List<Category> getAllCategory() {
         return categoryRepository.getCategoryStorage();
     }
 
+    // TODO: switch to H2
     @RequestMapping(value = "/reserve/{id}", method = RequestMethod.POST)
-    public Room reserveARoomByCategoryId(@PathVariable("id") int id){
+    public Room reserveARoomByCategoryId(@PathVariable("id") int id) {
         return roomStorage.reserveARoom(id);
     }
 }
