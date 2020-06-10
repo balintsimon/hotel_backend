3e776cfca18b23ef9448b349d9284c90f1d92270
Gnorbi951 <gnorbi951@gmail.com>
2020-06-10 18:16:48 +0200

    Write a query for Barna's request

--- src/main/java/com/codecool/hotel_backend/repository/ReservedRoomRepository.java
+++ src/main/java/com/codecool/hotel_backend/repository/ReservedRoomRepository.java
@@ -2,10 +2,13 @@ package com.codecool.hotel_backend.repository;
 
 import com.codecool.hotel_backend.entity.ReservedRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ReservedRoomRepository extends JpaRepository<ReservedRoom, Long> {
 
+    // This is utterly idiotic but works
+    @Query("select res, roo, reserv from ReservedRoom res join res.room roo join res.reservation reserv")
     List<ReservedRoom> findAll();
 }
